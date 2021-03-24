cf3b40577ccfc6ed356a61a270918a1b7925c728
Kyle Z <kyle1001001@gmail.com>
2021-03-24 11:42:23 -0700

    Day 3

--- tests/helloworld.py
+++ tests/helloworld.py
@@ -1,19 +1,11 @@
-# import Flask objects
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
-# http://127.0.0.1:5000/kyle/?name=zhang&num=1234
-@app.route('/kyle/')
-def hello_kyle():
-    params = request.args
-    return f'Hello, kyle! Your parameters are : {params}'
-
-# http://127.0.0.1:5000/kai/?name=zhang&num=1234
-@app.route('/kai/')
-def hello_kai():
-    params = request.args
-    return f'Hello, kai! Your last name is : {params.get("name")}'
+@app.route('/')
+def index():
+    return app.send_static_file("BestValueSchools.htm")
+    #return render_template('BestValueSchools.htm')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(host='127.0.0.1', port=8000, debug=True)
+ 
