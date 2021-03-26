ceacb1f5e97707b00f46701221b2927262e6edc9
Kyle Z <kyle1001001@gmail.com>
2021-03-26 12:13:17 -0700

    Ver Day4

--- Day2/day2Presentation/crawlerAutismStudents2.py
+++ Day2/day2Presentation/crawlerAutismStudents2.py
@@ -22,18 +22,65 @@ def retrieveData(api_url):
     soup = BeautifulSoup(html, 'html.parser')
 
     parentClass = soup.findChild("div", class_="entry-content clearfix")
-    ranking_AND_names = parentClass.findChildren("h3")
-    ranking_AND_names.pop(0)
-    ranking_AND_names.pop(len(ranking_AND_names)-1)
-    # print(ranking_AND_names)
-    location = parentClass.findChildren("h4")
-    # print(location)
-    pList_Link_Desc = parentClass.findChildren('p')
-    datalist = []
-    for i in range(0, 17):
-        pList_Link_Desc.pop(0)
-    # print(pList_Link_Desc)
+
+    children = parentClass.findChildren()
+    print(children)
+    h3Children = children.findChildren("h3")
+    for i in range(0, len(children)):
+        if (children[i] in h3Children): #and children[i] contains number at front
+            #try if children[i].getText()[0:1] contains number in front using int(children[i]) and except ValueError (if value error, just "continue" on the loop)
+            #do a cycle (get name [i], location[i+1]...)
+            #post check if children[i] in h3Children for descripion
+
+    # ranking_AND_names = parentClass.findChildren("h3")
+    # ranking_AND_names.pop(0)
+    # ranking_AND_names.pop(len(ranking_AND_names)-1)
+    # # print(ranking_AND_names[0])
+    # locationList = parentClass.findChildren("h4")
+    # # print(location)
+    # pList_Link_Desc = parentClass.findChildren('p')
+    # print("Length of pList:", len(pList_Link_Desc))
+    # datalist = []
+    # for i in range(0, 17):
+    #     pList_Link_Desc.pop(0)
+    # # print(pList_Link_Desc)
+    # # 34, 35, 36
+    # j = 0
     # for i in range(0, len(ranking_AND_names)):
+    #     row = []
+    #     ranking_name = ranking_AND_names[i].getText().split(". ")
+    #     ranking = ranking_name[0]
+    #     univ_name = ranking_name[1]
+
+    #     row.append(univ_name)
+    #     location = locationList[i].getText()
+    #     row.append(location)
+    #     row.append(ranking)
+    #     url = pList_Link_Desc[j].findChild('a')["href"]
+        
+    #     desc = ""
+    #     desc += pList_Link_Desc[j + 1].getText()
+    #     while (pList_Link_Desc[j + 2].findChild('a') is None or len(pList_Link_Desc[j + 2]) > 30):
+    #         desc += pList_Link_Desc[j + 2].getText()
+    #         j += 1
+    #     j += 2    
+    #     row.append(desc)
+    #     row.append(url)
+    #     row.append(api_url)
+    #     print(row)
+
+
+    #     ### the initial way of thought for extracting univ_name and ranking
+    #     # if (i < 10):
+    #     #     # "8. UniversityName"
+    #     #     univ_name = ranking_AND_names[i][3:]
+    #     #     ranking = ranking_AND_names[i][0:1]
+    #     # else:
+    #     #     # "12. UniversityName"    
+    #     #     univ_name = ranking_AND_names[i][4:]
+    #     #     ranking = ranking_AND_names[i][0:2]
+        
+
 
 retrieveData(api_url)
 
